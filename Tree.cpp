e574abd515979c4b0f54a951e00c450505aa99ce
Ketosvaldo <ketchulo.conk2013@gmail.com>
2023-11-21 12:50:35 -0600

    tree class

---
+++ Tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alumnos on 21/11/2023.
+//
+
+#include "Tree.h"
---
+++ Tree.h
@@ -0,0 +1,14 @@
+//
+// Created by Alumnos on 21/11/2023.
+//
+
+#ifndef GITTUTORIAL_TREE_H
+#define GITTUTORIAL_TREE_H
+
+
+class Tree {
+
+};
+
+
+#endif //GITTUTORIAL_TREE_H
